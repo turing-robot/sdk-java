3c42d2bf7093098891566e743f7a1538719124f5
魏志文 <weizhiwen@uzoo.cn>
2016-11-11 17:05:29 +0800

    fix

--- src/test/java/test/com/turingrobot/sdk/service/impl/TestOpenapiServieImpl.java
+++ src/test/java/test/com/turingrobot/sdk/service/impl/TestOpenapiServieImpl.java
@@ -1,13 +1,15 @@
 package test.com.turingrobot.sdk.service.impl;
 
+import org.junit.Test;
+
 /**
  * Test - openapi语义解析接口
  *
  * @author TURINGROBOT Team
  * @version 1.0
  */
-public class TestOpenapiServiceImpl {
+public class TestOpenapiServieImpl {
+    @Test
     public void testWebapi() {
-
     }
 }
